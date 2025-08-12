c68baaa15603d4db4c44a2f8862220e196fd3398
Sazzad Hossin <sazzadhossinsojib21@gmail.com>
2025-08-12 10:45:43 +0600

    Adding full variable page

--- no-2_variable.py
+++ no-2_variable.py
@@ -2,3 +2,19 @@
 a = 10
 print(a)
 print(id(a))
+
+#no2
+name = "Sazzad"
+print(name)
+
+#no3
+id = 2230035
+print(id)
+
+#no4
+fatherName = "Md.Zamerul Islam"
+print(fatherName)
+
+#no5
+mother_name = "Mst.Sheuli Begum"
+print(mother_name)
