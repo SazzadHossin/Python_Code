d662e5d8eb1b050dfa2aa6af61b13a833ff8b5f8
Sazzad Hossin <sazzadhossinsojib21@gmail.com>
2025-10-07 14:49:50 +0600

    Initial Commit

---
+++ no-10_while-loop.py
@@ -0,0 +1,13 @@
+#Loop Control Statement
+'''Loop Control Statement are used when a section of code may either be executed a fixed number of times or while some conditions are true
+1.while loop
+2.for loop
+'''
+
+#while loop: The while loop keep repeating an action until an associated condition returns false.
+a = 1
+
+while a<=10:
+    print(a)
+    a += 1
+print('Rest of the code')
