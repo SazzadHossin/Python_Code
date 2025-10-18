63175507e11f5b3d6f17ef569a5d40b5a7aec64d
Sazzad Hossin <sazzadhossinsojib21@gmail.com>
2025-10-18 22:37:27 +0600

    initial commit

---
+++ no-18_numpy.py
@@ -0,0 +1,7 @@
+#numpy introduction
+
+'''In python numpy is a packages which contains classes,functions,variable,library of mathmetical functions etc to work with scientific calculation.
+numpy can be used to create n dimensional arrays where n is any integer.We can create 1 dimensional array, 2 dimensional array, 3 diamensional array and so on.
+numpy's arrays classes is called ndarray.It also known by alias name array.There is another class in python which is different form numpy's arrays class.
+'''
+import numpy as np
